2a5dd4497288f23c4cb7a773c84cc24b3ecfa305
Алексей Зорин <zorinalexey59292@gmail.com>
2022-11-17 12:17:40 +0400

    1.0.1

--- src/FileSystem.php
+++ src/FileSystem.php
@@ -12,12 +12,43 @@ namespace CloudCastle\FileSystem;
  * @author Зорин Алексей <zorinalexey59292@gmail.com>
  * @copyright 2022 разработчик Зорин Алексей Евгеньевич.
  */
-class FileSystem
+abstract class FileSystem
 {
 
+    /**
+     * Объект для работы с файлами
+     *
+     * @var File|null
+     */
+
     public ?File $file;
+
+    /**
+     * Объект для работы с папками
+     *
+     * @var Dir|null
+     */
     public ?Dir $dir;
+
+    /**
+     * Объект для работы с json файлами
+     *
+     * @var Json|null
+     */
     public ?Json $json;
+
+    /**
+     * Объект для работы с сылками
+     *
+     * @var Link|null
+     */
+    public ?Link $link;
+
+    /**
+     * Коллекция объектов класса
+     *
+     * @var array
+     */
     protected static array $obj = [];
 
     /**
@@ -28,6 +59,7 @@ class FileSystem
         $this->dir = new Dir();
         $this->file = new File();
         $this->json = new Json();
+        $this->link = new Link();
     }
 
     /**
@@ -38,7 +70,7 @@ class FileSystem
     {
         $class = get_called_class();
         if ( ! isset(self::$obj[$class])) {
-            self::$obj[$class] = new self();
+            self::$obj[$class] = new $class();
         }
         return self::$obj[$class];
     }
---
+++ src/Link.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace CloudCastle\FileSystem;
+
+/**
+ * Класс Link
+ * @version 0.0.1
+ * @package CloudCastle\FileSystem
+ * @generated Зорин Алексей, please DO NOT EDIT!
+ * @author Зорин Алексей <zorinalexey59292@gmail.com>
+ * @copyright 2022 разработчик Зорин Алексей Евгеньевич.
+ */
+class Link
+{
+
+}
